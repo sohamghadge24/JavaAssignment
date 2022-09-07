82e2e83ba2b3415756ee798b3f8e7dee6b1568ea
sohamghadge24 <65226596+sohamghadge24@users.noreply.github.com>
2022-09-07 16:07:50 +0530

    Add files via upload

---
+++ Daily Code/factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class factorial{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int x = sc.nextInt();
+        fact(x);
+    }
+    static void fact(int a){
+        int num=1;
+        for(int i=1;i<=a;i++){    
+            num=num*i;    
+        }    
+        //System.out.print(num);
+        System.out.print("Factorial of "+a+" is : "+num);
+    }
+}
