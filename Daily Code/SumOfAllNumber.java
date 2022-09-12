f1a430d858375561c577cfba0b647ea02666d2b0
sohamghadge24 <65226596+sohamghadge24@users.noreply.github.com>
2022-09-12 17:24:38 +0530

    Update SumOfAllNumber.java

--- Daily Code/SumOfAllNumber.java
+++ Daily Code/SumOfAllNumber.java
@@ -3,13 +3,30 @@ import java.util.Scanner;
 public class SumOfAllNumber {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-            System.out.print("Enter starting Number: ");
-            int a = sc.nextInt();
-            System.out.print("Enter ending  Number: ");
-            int b = sc.nextInt();
-             sum(a,b);
-           
+            int sum=0;
+            System.out.println("1.Number of your choose");
+            System.out.println("2.Number in line");
+            System.out.print("Enter Number : ");
+            int x = sc.nextInt();
+            switch(x){
+                case 1:System.out.print("Enter how many number: ");
+                int a = sc.nextInt();
+                for(int i=0;i<a;i++){
+                    System.out.print("Enter  Number: ");
+                    int b = sc.nextInt(); 
+                    sum+=b;
+                }
+                System.out.println("Sum of Numbers is "+sum);
+                break;
+                case 2:System.out.print("Enter starting Number: ");
+                int s = sc.nextInt();
+                System.out.print("Enter ending  Number: ");
+                int y = sc.nextInt();
+                sum(s,y);
+                
+            }
     }
+    
     static void sum(int a, int b) {
         int sum=0;
         for(int i=a;i<=b;i++){
