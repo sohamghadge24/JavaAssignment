279dbbd1b512cc4386db8836d122336e8dbd293c
sohamghadge24 <65226596+sohamghadge24@users.noreply.github.com>
2022-09-13 18:09:40 +0530

    Update PerfectNumber.java

--- Daily Code/PerfectNumber.java
+++ Daily Code/PerfectNumber.java
@@ -4,28 +4,29 @@ public class PerfectNumber {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int count=0;
+        int totalcount=0;
         PerfectNumber obj =new PerfectNumber();
         System.out.print("Enter lower limit: ");
         int a = sc.nextInt();
         System.out.print("Enter upper limit: ");
         int b = sc.nextInt(); 
         for(int i=a;i<=b;i++){
-            count=obj.num(i,count);
+            if(a!=0&&i!=1)
+                count=obj.num(i,count);   //method call 
         }
-         System.out.print("Perfect Number count : "+count);
+         System.out.print("Perfect Number count : "+count); //total count 
        
     }
-    int num(int a,int count){
+    int num(int a,int count){   //
         int sum=1;
         for (int i = 2; i < a; i++) {
             if (a % i == 0) {
                 sum += i;
             }
-            
         }
-        if (sum == a)
-             count+=1;
-
+        if (sum == a){
+            count+=1;
+        }
         return count;
     
     }
