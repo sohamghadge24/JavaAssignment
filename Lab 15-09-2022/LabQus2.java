da69431b48a8aab099e4a162bf0cb22206d1da4a
sohamghadge24 <65226596+sohamghadge24@users.noreply.github.com>
2022-09-15 16:20:45 +0530

    Update LabQus2.java

--- Lab 15-09-2022/LabQus2.java
+++ Lab 15-09-2022/LabQus2.java
@@ -32,10 +32,32 @@ public class LabQus2 {
 
 
 /*
- *  output :
- 0 Soham 0 Manager
-349 null 100000 null
-0 Aayush 0 Developer
-100 null 250000 null
-1002 soham 500000 Developer
+ * Enter starting number: 1
+Enter Ending number: 100
+1 is a prime number
+2 is a prime number
+3 is a prime number
+5 is a prime number
+7 is a prime number
+11 is a prime number
+13 is a prime number
+17 is a prime number
+19 is a prime number
+23 is a prime number
+29 is a prime number
+31 is a prime number
+37 is a prime number
+41 is a prime number
+43 is a prime number
+47 is a prime number
+53 is a prime number
+59 is a prime number
+61 is a prime number
+67 is a prime number
+71 is a prime number
+73 is a prime number
+79 is a prime number
+83 is a prime number
+89 is a prime number
+97 is a prime number
  */
