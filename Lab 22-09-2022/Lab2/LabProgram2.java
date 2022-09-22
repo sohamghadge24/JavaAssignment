82680bf5457e64501e4a7c92541efd2084398cc8
sohamghadge24 <65226596+sohamghadge24@users.noreply.github.com>
2022-09-22 16:56:20 +0530

    Create LabProgram2.java

---
+++ Lab 22-09-2022/Lab2/LabProgram2.java
@@ -0,0 +1,32 @@
+package lab2;
+/*
+2. Write program to implement multilevel as well as Hierarchical inheritance in a same program.	
+
+*/
+public class LabProgram2 extends Child2{
+	public static void main(String[] args) {
+	new Child3();
+	}
+}
+/*
+flow of code
+								    	parent 
+                        |
+           ______________________________           
+					|			  	    |               |
+			child1					child2				  child3
+			                  |
+									  labprogram2
+
+*/
+
+
+
+/*
+ output: 
+		Family-member :2
+		Parent
+		Family-member :5
+		Child3
+ */
+
