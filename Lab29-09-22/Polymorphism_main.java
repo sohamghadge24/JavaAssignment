85caf875a43f564bf3a0b983b001a5a6cd4b14ff
sohamghadge24 <65226596+sohamghadge24@users.noreply.github.com>
2022-09-29 16:52:14 +0530

    Create Polymorphism_main.java

---
+++ Lab29-09-22/Polymorphism_main.java
@@ -0,0 +1,13 @@
+package labPractical;
+
+//Quest3
+public class Polymorphism_main {
+	public static void main(String[] args) {
+		//String name,int ram,String color
+		
+		AndroidPhone[] arr =new AndroidPhone[3];
+		arr[0]=new AndroidPhone("Apple", 8 ,"Space Gray");
+		arr[1]=new AndroidPhone("MI", 6 ,"Black");
+		arr[2]=new AndroidPhone("Samsung", 6 ,"white");
+	}
+}
