9c554f810440876778fca05c4dbed6a27b4ef602
sohamghadge24 <65226596+sohamghadge24@users.noreply.github.com>
2022-10-07 11:17:45 +0530

    Create Infor_Main.java

---
+++ Lab7-10-2022/Question 1/Infor_Main.java
@@ -0,0 +1,24 @@
+
+
+public class Infor_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person obj =new Emp();
+		obj.person(1,"Soham");
+		
+		obj=new Student();
+		obj.person(2,"Aayush");
+	}
+
+/*
+output:
+    Employee Id: 1
+    Employee Name: Soham
+    Employee Salary:9000000
+
+    Student Id: 2
+    Student Name: Aayush
+    Student total Marks :90
+    */
+  
