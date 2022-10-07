08e4a88b1fd59f27968689bcd924daebd9433202
sohamghadge24 <65226596+sohamghadge24@users.noreply.github.com>
2022-10-07 11:40:08 +0530

    Update Main_Bank.java

--- Lab7-10-2022/Question 2/Main_Bank.java
+++ Lab7-10-2022/Question 2/Main_Bank.java
@@ -1,6 +1,7 @@
 
 public class Main_Bank {
-	public static void main(String[] args) {
+	public static void main(String[] args) { //main method 
+		
 		Bank obj=new Sbi();
 		obj.interest(100000);
 		
@@ -11,3 +12,19 @@ public class Main_Bank {
 		obj.interest(100);
 	}
 }
+
+
+/*
+output:
+	Money:100000
+	Interest:7
+	Money(with interest):107000
+
+	Money:500000
+	Interest:9
+	Money(with interest):545000
+
+	Money:100
+	Interest:8
+	Money(with interest):108
+*/
