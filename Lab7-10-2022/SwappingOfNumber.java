ab8340ae76b07080572293a0fd761e7faec58a4d
sohamghadge24 <65226596+sohamghadge24@users.noreply.github.com>
2022-10-07 11:50:51 +0530

    Create SwappingOfNumber.java

---
+++ Lab7-10-2022/SwappingOfNumber.java
@@ -0,0 +1,31 @@
+package Assignment2;
+
+public class SwappingOfNumber {                       //Question 3
+	public static void main(String[] args) {
+		SwappingOfNumber obj =new SwappingOfNumber();
+		obj.ForDouble(200);
+		obj.ForInt(100);
+	}
+	
+	void ForInt(int a){
+		System.out.println("integers: "+a);
+		double b=a;
+		System.out.println("Swapping with Double :"+b+"\n");
+	}
+	
+	void ForDouble(double a){
+		System.out.println("integers: "+a);
+		int b=(int) a;
+		System.out.println("Swapping with Double :"+b +"\n");
+		}
+}
+
+/*
+output:
+      integers: 200.0
+      Swapping with Double :200
+
+      integers: 100
+      Swapping with Double :100.0
+
+*/
