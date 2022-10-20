f19fa53e6f92af15bed035ce7c80ce9c3655f238
sohamghadge24 <65226596+sohamghadge24@users.noreply.github.com>
2022-10-20 09:31:12 +0530

    Create HashTreeProgram.java

---
+++ HomeAssignment/19-10-2022/HashTreeProgram.java
@@ -0,0 +1,64 @@
+package Assignment2;
+import java.util.*;
+
+//Implement a program with "hashSet and TreeSet "Class
+
+public class HashTreeProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashTreeProgram obj=new HashTreeProgram();
+		obj.HashSet();
+		System.out.println();
+		obj.TreeSet();
+	}
+	void HashSet(){
+		HashSet<String> hash=new HashSet<String>();
+		hash.add("Soham");
+		hash.add("has");
+		hash.add("good");
+		hash.add("health");
+		hash.add("and ");
+		hash.add("wealth");
+		
+		System.out.println("HashSet : ");
+		for(String s:hash) {
+			System.out.println(s);
+		}
+	}
+	void TreeSet(){
+		TreeSet<String> tree =new TreeSet<String>();
+		tree.add("null");
+		
+		tree.add("Soham");
+		tree.add("Good");
+		tree.add("boy");
+		tree.add("pretty girl");
+		tree.add("number");
+		System.out.println("TreeSet : ");
+		for(String s:tree) {
+			System.out.println(s);
+		}
+	}
+
+}
+
+/*
+output:
+        HashSet : 
+        wealth
+        and 
+        health
+        has
+        Soham
+        good
+
+        TreeSet : 
+        Good
+        Soham
+        boy
+        null
+        number
+        pretty girl
+
+*/
